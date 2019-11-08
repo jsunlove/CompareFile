9db611dce64229ca3d91dd4b5688c2ddfb4084bc
jsunlove <972459714@qq.com>
2019-11-08 17:51:13 +0800

    第一次提交

---
+++ src/com/jsun/Util/compareFile.java
@@ -0,0 +1,13 @@
+package com.jsun.Util;
+
+/**
+ * 比较两个文件夹下的所有文件是否相同
+ * 这是为了对比现在只考虑比较文件大小，文件内容不考虑，默认的使用环境是比较打的两个包是否有不一样的情况
+ * @author yssSWC
+ *
+ */
+public class compareFile {
+
+	
+	
+}
---
+++ src/com/jsun/Util/test.java
@@ -0,0 +1,5 @@
+package com.jsun.Util;
+
+public class test {
+
+}
